37d25e9457f5440920380931415a4b1a9eb7f612
wellsj9580 <99290983+wellsj9580@users.noreply.github.com>
2022-05-02 00:20:08 +0000

    ticket snacks

--- 11_tickets_snacks_v1.py
+++ 11_tickets_snacks_v1.py
@@ -1,8 +1,9 @@
 import pandas
 
-# initialise snack lists..... 
+# Initialise data-frame lists... 
 
 all_names = ["Rangi","Manaia", "Talia", "Arihi", "Fetu"]
+all_tickets = [7.5, 10.5, 10.5, 10.5, 6.5]
 
 popcorn = []
 mms = []
@@ -12,14 +13,24 @@ orange_juice = []
 
 snack_lists = [popcorn,mms, pita_chips, water, orange_juice]
 
-# Data Frame Dictionary 
 movie_data_dict = {
-  'Name': all_names, 
+  'Name': all_names,
+  'Ticket': all_tickets, 
   'Popcorn': popcorn,
   'Water': water, 
   'Pita Chips': pita_chips, 
   'M&Ms': mms,
   'Orange Juice': orange_juice
+}
+
+# cost of each snack 
+price_dict = {
+  'Popcorn': 2.5,
+  'Water': 2,
+  'Pita Chips': 4.5,
+  'M&Ms':3,
+  'Orange Juice': 3.25
+  
 }
 
 test_data =[
@@ -51,14 +62,20 @@ for client_order in test_data:
       add_list[-1] = amount 
 
 print ()
-print ("Names:", all_names)
-print("Popcorn: ", snack_lists[0])
-print("M&Ms:", snack_lists[1])
-print("Pita Chips: ", snack_lists[2])
-print("Water: ",snack_lists[3])
-print("Orange Juice: ", snack_lists[4])
 
-#print details.. 
-movie_frame = pandas.DataFrame (movie_data_dict)
+# Creat dataframe and set index to name column 
+movie_frame = pandas.DataFrame(movie_data_dict)
 movie_frame = movie_frame.set_index('Name')
-print (movie_frame)
+
+# Creates column called 'Sun Total' 
+# Fill it price fro snacks and ticket 3
+
+movie_frame["Sub Total"] = \
+    movie_frame['Ticket'] + \
+    movie_frame['Popcron']*price_dict['Popcorn'] + \
+    movie_frame['Water']*price_dict['Water'] + \
+    movie_frame['Pita Chips']*price_dict['Pita Chips'] + \
+    movie_frame['M&Ms']*price_dict['M&Ms'] + \
+    movie_frame['Orange Juice']*price_dict['Orange Juice']
+
+print(movie_frame)
